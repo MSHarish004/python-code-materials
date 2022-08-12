037ba1d61bc97293695bb06fa09ea736e8cc17ba
MSHarish004 <102298941+MSHarish004@users.noreply.github.com>
2022-08-12 21:48:15 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,23 @@
+n=10
+n1=20
+print(type(n))
+f=4.4454555
+print(type(f))
+b=True
+print(type(b)) 
+name='sai'
+print(name) 
+print(type(name))
+print(id(name)) 
+print(len(name))
+print(round(f,4))
+#using built-in func 
+#bin() func
+no=bin(20)
+print(no) 
+#oct() func
+no=oct(20)
+print(no) 
+#hex() func
+no=hex(20)
+print(no)  
