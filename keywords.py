145a19a1daaaff0912546206889328cd72bdd94e
MSHarish004 <102298941+MSHarish004@users.noreply.github.com>
2022-08-12 21:47:12 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,21 @@
+import keyword 
+keyword.kwlist 
+print(keyword.kwlist)
+print('python')
+print(100/3)
+print(100//3)
+print(3**2)
+print(100%2)
+
+
+print(2**3+(2*3))
+print((2**3+2)*3)
+#identifier
+doorno=41 #doorno=identier
+print(doorno)
+name='saiharish'
+print(name) 
+tamil1=76 #alphabets all identifier
+tamil_1=67#alphabet with numbers and underscore all identifier 
+_maths_=80
+print(_maths_)
